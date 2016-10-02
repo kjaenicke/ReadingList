7a9218c19721a0479767cda6ab9c0506fe5e8cfb
Keenan Jaenicke <kjaenick@gmail.com>
2016-10-02 18:21:29 -0400

    adds basic search

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,6 +4,15 @@ class Book < ActiveRecord::Base
 
   scope :finished, -> { where.not(finished_on: nil) }
   scope :recent, -> { where('finished_on > ?', 2.days.ago) }
+  scope :search, ->(keyword) { where(title: keyword) if keyword.present?  }
+
+  # def self.search(keyword)
+  #   if keyword.present?
+  #     where(title: keyword)
+  #   else
+  #     all
+  #   end
+  # end
 
   def finished?
     finished_on.present?
