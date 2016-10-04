2fdc28cdbb001529b7f39ff588a2de2618dacd62
Keenan Jaenicke <kjaenick@gmail.com>
2016-10-04 14:51:43 -0400

    adds saving books

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,17 +2,11 @@ class Book < ActiveRecord::Base
   has_many :book_genres
   has_many :genres, through: :book_genres
 
-  scope :finished, -> {
-    where.not(finished_on: nil)
-  }
-  scope :recent, -> {
-    where('finished_on > ?', 2.days.ago)
-  }
-  scope :search, -> (keyword) {
-    where('keywords LIKE ?', "%#{keyword.downcase}%") if keyword.present?
-  }
-  scope :filter, -> (genreName) {
-    joins(:genres).where('genres.name = ?', genreName) if name.present?
+  scope :finished, -> { where.not(finished_on: nil) }
+  scope :recent, -> { where('finished_on > ?', 2.days.ago) }
+  scope :search, -> (keyword){ where('keywords LIKE ?', "%#{keyword.downcase}%") if keyword.present? }
+  scope :filter, -> (name){
+    joins(:genres).where('genres.name = ?', name) if name.present?
   }
 
   before_save :set_keywords
